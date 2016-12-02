236fe23e0421e0e3cc348dbcf59106791a209e15
wadasiwalidesu1 <3361508739@qq.com>
2016-12-02 17:49:24 +0800

    update memory configuration file "memory.properties"

--- Memory/src/main/java/mycat/leaderus/lzy/cachesys/memcache_v5/MemConfig.java
+++ Memory/src/main/java/mycat/leaderus/lzy/cachesys/memcache_v5/MemConfig.java
@@ -1,11 +1,35 @@
 package mycat.leaderus.lzy.cachesys.memcache_v5;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by 行知道人 on 2016/11/29.
  */
 public class MemConfig {
-    public static final int SLAB_SIZE = 1024*1024;
-    public static final int TOTAL_MOMORY_SIZE = 100 * SLAB_SIZE;
-    public static final int CHUNK_SIZES = 256;
+    public static final int SLAB_SIZE;
+    public static final int TOTAL_MEMORY_SIZE;
+    public static final int CHUNK_SIZES;
+
+    static {
+        Properties properties = new Properties();
+        try {
+            properties.load(MemConfig.class.getResourceAsStream("/memory.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int slab_size = Integer.parseInt(properties.getProperty("SLAB_SIZE"));
+        int total_memory_size = Integer.parseInt(properties.getProperty("TOTAL_MEMORY_SIZE"));
+        int chunk_size = Integer.parseInt(properties.getProperty("CHUNK_SIZES"));
+        if (slab_size < total_memory_size >>> 3 && slab_size >>> 3 > chunk_size && chunk_size > 0) {
+            SLAB_SIZE = slab_size;
+            TOTAL_MEMORY_SIZE = total_memory_size;
+            CHUNK_SIZES = chunk_size;
+        } else {
+            SLAB_SIZE = 1048576;
+            TOTAL_MEMORY_SIZE = 134217728;
+            CHUNK_SIZES = 256;
+        }
+    }
 
 }
