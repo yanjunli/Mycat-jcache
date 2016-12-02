ab60356bd8a7d03996c34fad1ef04809d21b55fa
wadasiwalidesu1 <3361508739@qq.com>
2016-12-02 15:22:56 +0800

    upload the referencs documentation

--- Mcache/src/main/java/io/mycat/mcache/command/BinaryGetKCommand.java
+++ Mcache/src/main/java/io/mycat/mcache/command/BinaryGetKCommand.java
@@ -1,15 +1,12 @@
 package io.mycat.mcache.command;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-import io.mycat.mcache.McacheGlobalConfig;
-import io.mycat.mcache.command.binary.ProtocolCommand;
 import io.mycat.mcache.command.binary.ProtocolResponseStatus;
 import io.mycat.mcache.conn.Connection;
 import io.mycat.mcache.conn.handler.BinaryProtocol;
-import io.mycat.mcache.conn.handler.BinaryRequestHeader;
 import io.mycat.mcache.conn.handler.BinaryResponseHeader;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * getk 命令 
