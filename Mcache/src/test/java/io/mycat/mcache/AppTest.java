989ceeae5d7b0348027993c28d2d8dcc456af654
mycat-j <allovertheword@163.com>
2016-12-05 00:34:23 +0800

    增加 nio 部分 性能压测 测试方法

--- Mcache/src/test/java/io/mycat/mcache/AppTest.java
+++ Mcache/src/test/java/io/mycat/mcache/AppTest.java
@@ -2,12 +2,16 @@ package io.mycat.mcache;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.whalin.MemCached.MemCachedClient;
 import com.whalin.MemCached.SockIOPool;
+
+import mycat.leaderus.lzy.cachesys.memcache_v5.ReadWritePool;
 
 /**
  * Unit test for simple App.
@@ -69,21 +73,21 @@ public class AppTest {
         System.out.println(">>> " + bar);
 	}
 
-//    @Test
-//    public void testReadWritePool() throws InterruptedException {
-//        ReadWritePool.set("123", 0, "123".getBytes().length, System.currentTimeMillis() + 1000, "123".getBytes());
-//        ReadWritePool.set("1234", 0, "1234".getBytes().length, System.currentTimeMillis() + 100, "123".getBytes());
-//        ReadWritePool.set("1235", 0, "1235".getBytes().length, System.currentTimeMillis() + 100000, "123".getBytes());
-//        ReadWritePool.set("1236", 0, "1236".getBytes().length, System.currentTimeMillis() + 100000, "123".getBytes());
-//        ReadWritePool.set("1237", 0, "1237".getBytes().length, System.currentTimeMillis() + 100000, "123".getBytes());
-//        System.out.println(new String(ReadWritePool.get(new String[]{"123"})[0].values));
-//        System.out.println(new String(ReadWritePool.get(new String[]{"1235"})[0].values));
-//        System.out.println(new String(ReadWritePool.get(new String[]{"1236"})[0].values));
-//        System.out.println(new String(ReadWritePool.get(new String[]{"1237"})[0].values));
-//        System.out.println(new String(ReadWritePool.get(new String[]{"1234"})[0].values));
-//        Thread.sleep(1000);
-//        //System.out.println(new String(ReadWritePool.get(new String[]{"123"})[0].values));
-//    }
+    @Test
+    public void testReadWritePool() throws InterruptedException {
+        ReadWritePool.set("123", 0, "123".getBytes().length, System.currentTimeMillis() + 1000, "123".getBytes());
+        ReadWritePool.set("1234", 0, "1234".getBytes().length, System.currentTimeMillis() + 100, "123".getBytes());
+        ReadWritePool.set("1235", 0, "1235".getBytes().length, System.currentTimeMillis() + 100000, "123".getBytes());
+        ReadWritePool.set("1236", 0, "1236".getBytes().length, System.currentTimeMillis() + 100000, "123".getBytes());
+        ReadWritePool.set("1237", 0, "1237".getBytes().length, System.currentTimeMillis() + 100000, "123".getBytes());
+        System.out.println(new String(ReadWritePool.get(new String[]{"123"})[0].values));
+        System.out.println(new String(ReadWritePool.get(new String[]{"1235"})[0].values));
+        System.out.println(new String(ReadWritePool.get(new String[]{"1236"})[0].values));
+        System.out.println(new String(ReadWritePool.get(new String[]{"1237"})[0].values));
+        System.out.println(new String(ReadWritePool.get(new String[]{"1234"})[0].values));
+        Thread.sleep(1000);
+        //System.out.println(new String(ReadWritePool.get(new String[]{"123"})[0].values));
+    }
 
     @Test
     public void testAddCommand(){
@@ -114,8 +118,59 @@ public class AppTest {
     
     @Test
     public void testgetMultiArray1(){
-    	 Object bars = mcc.keyExists("foo");
-    	 System.out.println(">>> " + bars);
+    	
+    	IntStream.range(0, 10).forEach(f->{
+    		Thread t = new Thread(new  Runnable() {
+				
+    			private Random random = new Random();
+				@Override
+				public void run() {
+					for(int i =0;i<100;i++){
+				    	MemCachedClient mcc = new MemCachedClient(true);  //true 代表 二进制协议，false 代表 文本协议
+						// 设置缓存服务器列表，当使用分布式缓存的时，可以指定多个缓存服务器。这里应该设置为多个不同的服务，我这里将两个服务设置为一样的，大家不要向我学习，呵呵。
+				        String[] servers =
+				                {
+				                        "127.0.0.1:9000"
+				                };
+
+				        // 设置服务器权重
+				        Integer[] weights = {3};
+
+				        // 创建一个Socked连接池实例
+				        SockIOPool pool = SockIOPool.getInstance();
+
+				      // 向连接池设置服务器和权重
+				        pool.setServers(servers);
+				        pool.setWeights(weights);
+
+				        // set some TCP settings
+				        // disable nagle
+				        // set the read timeout to 3 secs
+				        // and don't set a connect timeout
+				        pool.setNagle(false);
+				        pool.setSocketTO(3000);
+				        pool.setSocketConnectTO(0);
+
+				       // initialize the connection pool
+				        pool.initialize();
+				    	
+				    	 Object bars = mcc.keyExists("foo");
+				    	 System.out.println(">>> " + bars);
+				    	 try {
+							Thread.sleep(random.nextInt(5000));
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			});
+    		t.start();
+    	});
+    	
+    	while(true){
+    		
+    	}
     }
 
 }
