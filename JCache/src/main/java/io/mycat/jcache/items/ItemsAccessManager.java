ea598e828ab5d761b03fff1c78a62ed17155d483
yanjunli <allovertheword@163.com>
2016-12-14 23:59:05 +0800

    rename class name

---
+++ JCache/src/main/java/io/mycat/jcache/items/ItemsAccessManager.java
@@ -0,0 +1,19 @@
+package io.mycat.jcache.items;
+
+/**
+ * 
+ * 
+ * @author liyanjun
+ *
+ */
+public class ItemsAccessManager {
+
+	/*
+	 * Returns an item if it hasn't been marked as expired,
+	 * lazy-expiring as needed.
+	 */
+//	long item_get( int nkey){
+//		
+//	}
+	
+}
